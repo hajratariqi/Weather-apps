b5aa1575c97fd09aa25c34e9bc33aa605d8e8cca
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-28 01:48:07 +0500

    changed api

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ let mainDiv = document.getElementById('main')
 
 const getData = () => {
 return new Promise((resolve,reject)=>{
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=c7a75b3fc1e0f8569dcb075cee2a6a30`)
+// fetch(`https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=c7a75b3fc1e0f8569dcb075cee2a6a30`)
+fetch('https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=c7a75b3fc1e0f8569dcb075cee2a6a30&units=metric')
+
 .then(res => res.json())
 .then(res => resolve(res))
 .catch(err => console.log(err))
@@ -16,7 +18,7 @@ getData()
     mainDiv.innerHTML = `
       <div>
         <h1>Weather in ${res.name}, ${res.sys.country}</h1>
-        <p>Temperature: ${res.main.temp - 273.15}°C</p>
+        <p>Temperature: ${res.main.temp}°C</p>
         <p>Weather: ${res.weather[0].description}</p>
         <p>Humidity: ${res.main.humidity}%</p>
         <p>Wind Speed: ${res.wind.speed} m/s</p>
